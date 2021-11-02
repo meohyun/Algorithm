9cd100238443567ef781988f8443940c658625c3
meohyun24 <84959523+meohyun@users.noreply.github.com>
2021-11-02 14:37:54 +0900

    Update and rename 12.py to programmers1.py

--- programmers1.py
+++ programmers1.py
@@ -6,11 +6,13 @@ win_nums = list(map(int,sys.stdin.readline().split()))
 
 def solution(lottos,win_nums):
     answer = [7,7]
+    count = 0
 
     for i in range(len(lottos)):
         if lottos[i] in win_nums:
             answer[0] -= 1
             answer[1] -= 1 
+            count += 1
 
         if lottos[i] == 0 :
             answer[0] -= 1
@@ -18,7 +20,11 @@ def solution(lottos,win_nums):
         if sum(lottos) == 0 :
             answer[0] = 1
             answer[1] = 6
+            count = 6
 
+    if count == 0:
+        answer[0] = 6
+        answer[1] = 6
 
     
     return answer
